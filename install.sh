72e688d2ba5b3bf752a0505cd56eed50e8559cd5
炜Sir(David Zhang) <geek@lzw.name>
2016-10-11 00:13:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-mkdir /data/ && cd /data && yum install git && git clone https://github.com/bkbabydp/shell.git
+mkdir /data/ && cd /data && yum install git -y && git clone https://github.com/bkbabydp/shell.git
