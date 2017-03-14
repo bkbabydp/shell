cca3253f638e5a206862b9fcacd8f7befef45f8e
炜先生(David Zhang) <geek@lzw.name>
2017-03-14 21:28:58 +0800

    d

--- install.sh
+++ install.sh
@@ -1,17 +1,27 @@
 #! /usr/bin/env bash
 
-echo "Creating /data..."
+declare basepath=$(cd `dirname "$0"`; pwd)
+cd "$basepath"
 
-if [ ! -d "/data" ]; then
-  mkdir /data
+declare root_name=data
+declare app_name=shell
+
+echo "Creating /$root_name..."
+
+if [ ! -d "/$root_name" ]; then
+  mkdir /$root_name
 fi
 
 echo "Installing git..."
 
 yum install -y git
 
-echo "Clone shell.git..."
+echo "Installing app..."
 
-cd /data && git clone https://github.com/bkbabydp/shell.git
+if [ ! -d "/$root_name/$app_name" ]; then
+  cd /$root_name && git clone https://github.com/bkbabydp/shell.git
+else
+  cd /$root_name/$app_name && git pull origin master
+fi
 
 echo "done."
